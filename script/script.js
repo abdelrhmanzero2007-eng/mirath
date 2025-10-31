7ab60ffe2fa67dee20e7654db53d7b3cbf60d11e
Abd Elrahman <abdelrhman.zero.2007@gmail.com>
2025-10-31 15:43:13 +0200

    add js

--- script/script.js
+++ script/script.js
@@ -1,38 +1,28 @@
 
 
-// let allMoney = prompt(" ادخل المبلغ الكلي: ");
-// console.log("المبلغ الكلي " + allMoney);
-// //wayfe\=======================================
-// let wayfe = allMoney / 8;
-// console.log("نصيب الزوجه" + wayfe);
-// let toDivide = allMoney - wayfe;
-// //=============================================
+let allMoney = prompt(" ادخل المبلغ الكلي: ");
+console.log("المبلغ الكلي " + allMoney);
+//wayfe\=======================================
+let wayfe = allMoney / 8;
+console.log("نصيب الزوجه" + wayfe);
+let toDivide = allMoney - wayfe;
+//=============================================
 
-// //boys\=========================================
-// let boys = (toDivide / 3) * 2;
-// let oneBoy = prompt(" ادخل عدد الاولاد");
-// console.log("عدد الاولاد = " + oneBoy);
-// console.log("نصيب الاولاد = " + boys);
-// console.log(" نصيب الولد الواحد  = " + boys / oneBoy);
-// //=============================================
+//boys\=========================================
+let boys = (toDivide / 3) * 2;
+let oneBoy = prompt(" ادخل عدد الاولاد");
+console.log("عدد الاولاد = " + oneBoy);
+console.log("نصيب الاولاد = " + boys);
+console.log(" نصيب الولد الواحد  = " + boys / oneBoy);
+//=============================================
 
-// //girls\========================================
-// let girls = toDivide / 3;
-// let oneGirl = prompt(" ادخل عدد البنات  ");
-// console.log("عدد البنات  = " + oneGirl);
-// console.log("نصيب البنات = " + oneGirl * boys / oneBoy / 2);
-// console.log(" نصيب البنت الواحده  = " + boys / oneBoy / 2);
-// //==============================================
-
-
-let Aga= prompt(" enter your ")
-
-if ( Aga >= 2013){
-    console.log("Hello ");
-    
-}
-else{
+//girls\========================================
+let girls = toDivide / 3;
+let oneGirl = prompt(" ادخل عدد البنات  ");
+console.log("عدد البنات  = " + oneGirl);
+console.log("نصيب البنات = " + oneGirl * boys / oneBoy / 2);
+console.log(" نصيب البنت الواحده  = " + boys / oneBoy / 2);
+//==============================================
 
 
-    console.log('no');
-}
+
