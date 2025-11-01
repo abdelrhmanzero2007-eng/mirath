32239918aeb45d062bdedf1b9e00b86970a8c00b
Abd Elrahman <abdelrhman.zero.2007@gmail.com>
2025-11-01 12:50:49 +0200

    /////////

--- script/script.js
+++ script/script.js
@@ -1,24 +1,24 @@
-// let allMoney = prompt(" ادخل المبلغ الكلي: ");
-// console.log("المبلغ الكلي " + allMoney);
-// //wayfe\=======================================
-// let wayfe = allMoney / 8;
-// console.log("نصيب الزوجه" + wayfe);
-// let toDivide = allMoney - wayfe;
-// //=============================================
+let allMoney = prompt(" ادخل المبلغ الكلي: ");
+console.log("المبلغ الكلي " + allMoney);
+//wayfe\=======================================
+let wayfe = allMoney / 8;
+console.log("نصيب الزوجه" + wayfe);
+let toDivide = allMoney - wayfe;
+//=============================================
 
-// //boys\=========================================
-// let boys = (toDivide / 3) * 2;
-// let oneBoy = prompt(" ادخل عدد الاولاد");
-// console.log("عدد الاولاد = " + oneBoy);
-// console.log("نصيب الاولاد = " + boys);
-// console.log(" نصيب الولد الواحد  = " + boys / oneBoy);
-// //=============================================
+//boys\=========================================
+let boys = (toDivide / 3) * 2;
+let oneBoy = prompt(" ادخل عدد الاولاد");
+console.log("عدد الاولاد = " + oneBoy);
+console.log("نصيب الاولاد = " + boys);
+console.log(" نصيب الولد الواحد  = " + boys / oneBoy);
+//=============================================
 
-// //girls\========================================
-// let girls = toDivide / 3;
-// let oneGirl = prompt(" ادخل عدد البنات  ");
-// console.log("عدد البنات  = " + oneGirl);
-// console.log("نصيب البنات = " + oneGirl * boys / oneBoy / 2);
-// console.log(" نصيب البنت الواحده  = " + boys / oneBoy / 2);
-// //==============================================
+//girls\========================================
+let girls = toDivide / 3;
+let oneGirl = prompt(" ادخل عدد البنات  ");
+console.log("عدد البنات  = " + oneGirl);
+console.log("نصيب البنات = " + oneGirl * boys / oneBoy / 2);
+console.log(" نصيب البنت الواحده  = " + boys / oneBoy / 2);
+//==============================================
 
